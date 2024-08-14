68b2ad67d4a2f2e4d6154d8ad19bdffad4c82def
zytronium <dastelljes@outlook.com>
2024-08-14 11:25:24 -0500

    fixed not recognizing anything as a number by removing last character from input

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(void)
 
 	printf("Guess a number between %d and %d.\n", INT_MAX, INT_MIN);
 	getline(&input, &size, stdin);
-	printf("%c", input[strlen(input)]);
+	input[strlen(input) - 1] = '\0';
 	printf("Your number: %s.\n", input);
 	if (isNumber(input))
 		guess = atoi(input);
