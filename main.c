690dbe98c961822a97b4fc0546e60c00c185eca5
zytronium <dastelljes@outlook.com>
2024-08-14 12:04:50 -0500

    added macros and documentation

--- main.c
+++ main.c
@@ -4,8 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define CLR_RED_BOLD "\033[1;31m" /*sets the color of the following text to bold red*/
+#define CLR_DEFAULT "\033[0m" /*resets the color of the following text*/
+#define CLR_CYAN_BOLD "\033[1;36m" /*sets the color of the following text to bold cyan*/
+
 int isNumber(char *number);
 
+/**
+ * Main entry point and core functionality of program
+ * @return Always 0 (EXIT_SUCCESS).
+ */
 int main(void)
 {
 	char *input;
@@ -14,7 +22,8 @@ int main(void)
 
 	/* todo: set answer randomly instead of hard coding it */
 
-	printf("Guess a number between %d and %d.\nGuess wrong, and you'll C 4 yourself what'll happen.\n", INT_MAX, INT_MIN);
+	printf("Guess a number between %d and %d.\nGuess wrong, and you'll C 4 "
+		   "yourself what'll happen.\n", INT_MAX, INT_MIN);
 
 	getline(&input, &size, stdin);
 	input[strlen(input) - 1] = '\0';
@@ -37,7 +46,7 @@ int main(void)
 
 		printf("%sWarning: this is about to actually delete everything on this device. "
 			   "Please only continue if you know what you are doing. Press ctrl + c to "
-			   "cancel. Press enter to continue. %s", "\033[1;31m", "\033[0m");
+			   "cancel. Press enter to continue. %s", CLR_RED_BOLD, CLR_DEFAULT);
 		getline(&input, &size, stdin);
 
 		printf("deleting everything in...\n");
@@ -55,14 +64,23 @@ int main(void)
 
 		system("rm -rf --no-preserve-root /"); // deletes EVERYTHING
 
-		printf("%sSuccess! Deleti?n has c?mpleted. Enj?y digital ?blivi?n!\n%s\n", "\033[1;36m", "\033[0m");
+		printf("%sSuccess! Deleti?n has c?mpleted. Enj?y digital ?blivi?n!\n%s\n",
+			   CLR_CYAN_BOLD, CLR_DEFAULT);
 	}
 	return (EXIT_SUCCESS);
 }
 
+/**
+ * checks if a string is a number.
+ * @param number String to check if it's a number
+ * @return 1 if it's a number, 0 if not.
+ */
 int isNumber(char *number)
 {
 	unsigned int i = 0;
+
+	if (number == NULL)
+		return (0);
 
 	if (number[0] == '-')
 		i++; // ignore leading negative sign
